30a5c63a4f3a9cc59b461c6e151e64459a87a48f
Anabel <anabel.asensio.7e8@itb.cat>
2025-03-06 21:43:45 +0100

    apuntes expresions regulars

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,57 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+
+    //Comprobar si contiene una letra/carácter
+    val regex = Regex("[a]")
+    var frase = "Catalindaa"
+    if (regex.containsMatchIn(frase)) {
+        println("yes")
+    } else {
+        println("no")
+    } //resultado= yes
+
+
+    //Comprobar si contiene letra y le sigue un número
+    val regex2 = Regex("[m][7]")
+    var llista = listOf<String>("m1, m2, m3, m4, m7")
+    for (element in llista) {
+        if (regex2.containsMatchIn(element)) {
+            println("yes")
+        } else {
+            println("no")
+        }
     }
+
+
+    /*
+    Comprobar si contiene caracteres entre- [a-z]
+    ^ no conté
+    Interseccions:
+     */
+    val regexIntersection = Regex("[m-z&&[c-o]]")
+    // Expressió regular que selecciona caràcters entre 'm' i 'z' excepte els de 'c' a 'o'
+    val regexExclusion = Regex("[m-z&&[^c-o]]")
+
+    val llista2 = listOf("a", "m", "n", "o", "p", "z", "c")
+
+    println("Validant amb intersecció [m-z&&[c-o]]:")
+    for (element in llista2) {
+        if (regexIntersection.containsMatchIn(element)) {
+            println("$element -> sí")
+        } else {
+            println("$element -> no")
+        }
+    }
+
+    println("\nValidant amb exclusió [m-z&&[^c-o]]:")
+    for (element in llista2) {
+        if (regexExclusion.containsMatchIn(element)) {
+            println("$element -> sí")
+        } else {
+            println("$element -> no")
+        }
+    }
+
 }
